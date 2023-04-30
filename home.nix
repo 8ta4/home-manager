daa6ac0d15a7ade263df462753a8d26ba96f578c
a <a@as-Virtual-Machine.local>
2023-04-30 14:00:14 -0700

    Enable zsh completion, autosuggestions and syntax highlighting

--- home.nix
+++ home.nix
@@ -83,6 +83,9 @@
 
   programs.zsh = {
     enable = true;
+    enableCompletion = true;
+    enableAutosuggestions = true;
+    enableSyntaxHighlighting = true;
     oh-my-zsh = {
       enable = true;
       plugins = [
