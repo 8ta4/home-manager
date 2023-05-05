2fa9364bfa67792fceed8dfd375fcac0676b3750
a <a@example.com>
2023-05-04 20:07:52 -0700

    Remove comments and cd command from install.sh

--- install.sh
+++ install.sh
@@ -55,10 +55,6 @@ nix-channel --add https://github.com/nix-community/home-manager/archive/master.t
 nix-channel --update
 nix-shell '<home-manager>' -A install
 
-# % brew shell-init: error retrieving current directory: getcwd: cannot access parent directories: No such file or directory Error: The current working directory doesn't exist, cannot proceed.
-# The error message occurs when the shell is unable to retrieve the current directory because it cannot access parent directories. This can happen if the current working directory has been deleted or moved.
-cd
-
 # Remove existing home-manager directory if it exists
 if [ -d "$HOME/.config/home-manager" ]; then
   rm -rf "$HOME/.config/home-manager"
