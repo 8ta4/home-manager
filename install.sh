d834c7ae3f34dce5ae6f61c2e6505cd9be2ca810
a <a@example.com>
2023-05-04 20:26:09 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,18 +2,10 @@
 
 set -e
 
-## Install Homebrew
-# Need sudo access on macOS
-# https://github.com/Homebrew/install/blob/fc8acb0828f89f8aa83162000db1b49de71fa5d8/install.sh#L228
 # This will prompt you for the password once, and then subsequent sudo commands will not require a password as long as the authentication is cached.
 sudo true
 
-# Install brew non-interactively using NONINTERACTIVE environment variable
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# Add Homebrew to your PATH
-eval "$(/opt/homebrew/bin/brew shellenv)"
- 
+## Install Nix via the recommended multi-user installation
 # Clean up the old backup file
 # https://github.com/NixOS/nix/blob/89d3cc5a47a448f624ea4c9b43eeee00dcc88a21/scripts/install-multi-user.sh#L36-L37
 readonly PROFILE_TARGETS=("/etc/bashrc" "/etc/profile.d/nix.sh" "/etc/zshrc" "/etc/bash.bashrc" "/etc/zsh/zshrc")
@@ -26,7 +18,6 @@ for target in "${PROFILE_TARGETS[@]}"; do
   fi
 done
 
-## Install Nix via the recommended multi-user installation
 yes | sh <(curl -L https://nixos.org/nix/install)
 
 # Nix won't work in active shell sessions until you restart them
@@ -46,10 +37,6 @@ if [ $retries -eq $max_retries ]; then
   exit 1
 fi
 
-## Build and install nix-darwin
-nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
-yes | ./result/bin/darwin-installer
-
 ## Install home-manager
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
@@ -63,9 +50,23 @@ fi
 # Clone GitHub repo to ~/.config/home-manager
 git clone https://github.com/8ta4/home-manager.git "$HOME/.config/home-manager"
 
-## Run home-manager switch and brew bundle
+# Run home-manager switch
 home-manager switch
 
+## Install nix-darwin
+nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
+yes | ./result/bin/darwin-installer
+
+## Install Homebrew
+# Need sudo access on macOS
+# https://github.com/Homebrew/install/blob/fc8acb0828f89f8aa83162000db1b49de71fa5d8/install.sh#L228
+# Install brew non-interactively using NONINTERACTIVE environment variable
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Add Homebrew to your PATH
+eval "$(/opt/homebrew/bin/brew shellenv)"
+ 
+# Run brew bundle
 # Change directory to cloned repo
 cd "$HOME/.config/home-manager"
 brew bundle
